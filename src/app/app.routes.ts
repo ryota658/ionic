b3b82fd6402f8092e0695c047ef6dbe2f85d5e79
Ryota658 <you@example.com>
2024-04-20 12:08:00 +0900

    feat: ログインしていない場合は、ログインページへ。

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,12 +1,14 @@
 import { Routes } from '@angular/router';
+import { AuthGuard } from './auth/guards/AuthGuard';
 
 export const routes: Routes = [
   {
     path: '',
     loadChildren: () => import('./tabs/tabs.routes').then((m) => m.routes),
+    canActivate: [AuthGuard]
   },
   {
     path: 'login',
-    loadComponent: () => import('./auth/login/login.page').then( m => m.LoginPage)
+    loadComponent: () => import('./auth/login/login.page').then(m => m.LoginPage)
   },
 ];
